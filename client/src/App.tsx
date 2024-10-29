26f27bb481b3d4473625d7ca9cb8c3b9254a57f3
Isyraf Fajar A <itopfajar@gmail.com>
2024-10-29 12:49:45 +0700

    menu login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,84 @@
 import { useState } from 'react'
-import './App.css'
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './components/ui/card';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './components/ui/form';
+import { Input } from './components/ui/input';
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
 
-function Counter({ initial }) {
-  const [count, setCount] = useState(initial);
-  return <div className="flex flex-row my-2">
-    {count > -10 ?
-      <Button onClick={() => setCount(count - 1)}>-</Button> :
-      <Button>Gak Bisa</Button>
-    }
-    <p className="grow">
-      {count == 0 && "Tengah"}
-      <span> </span>
-      Jumlah {count}
-    </p>
-    {count < 10 ?
-      <Button onClick={() => setCount(count + 1)}>+</Button> :
-      <Button>Gak Bisa</Button>
-    }
-  </div>
-}
-
-const initials = [0, 1];
+const loginSchema = z.object({
+  password: z.string({
+    message: "tidak boleh kosong"
+  }).min(8, {
+    message: "minimal 8 karakter"
+  })
+  , username: z.string({
+    message: "tidak boleh kosong"
+  }).min(6, {
+    message: "minimal 6 karakter"
+  })
+})
 
 function App() {
-  return initials.map(int => <Counter key={int} initial={int} />)
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: ""
+    }
+  });
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  return <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <Card className='max-w-96 mx-auto'>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) =>
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input {...field}>
+                  </Input>
+                </FormControl>
+                <FormDescription>
+                  aku suka auladi
+                </FormDescription>
+                <FormMessage />
+              </FormItem>} />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) =>
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type='password' {...field}>
+                  </Input>
+                </FormControl>
+                <FormDescription>
+                  aku suka auladi
+                </FormDescription>
+                <FormMessage />
+              </FormItem>} />
+        </CardContent>
+        <CardFooter>
+          <Button type='submit'>Login</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  </Form>
 }
 
 export default App
