7f677cd81f7b3993daf7d864f9c8231f5895d6f3
uR <fahri.aulady101@gmail.com>
2024-11-11 16:15:57 +0700

    Update premi

--- apps/client/src/pages/DashboardPage/Premi.tsx
+++ apps/client/src/pages/DashboardPage/Premi.tsx
@@ -1,5 +1,10 @@
-import * as React from 'react'
+import { useState } from 'react';
+import { Input } from '@client/components/ui/input';
+import { Button } from '@client/components/ui/button';
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from '@client/components/ui/card';
 
 export function PremiPage() {
-    return <h1>a</h1>
+    return {
+        
+    }
 }
