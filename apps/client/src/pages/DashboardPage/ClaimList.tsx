d88273c5b1d1a15b4f4dffc050a22175c696e597
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-11-12 09:48:56 +0700

    make claim design better

--- apps/client/src/pages/DashboardPage/ClaimList.tsx
+++ apps/client/src/pages/DashboardPage/ClaimList.tsx
@@ -1,29 +1,62 @@
 import { useClaimList } from "@client/api/claim"
+import { Badge, BadgeProps } from "@client/components/ui/badge";
 import { Button } from "@client/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@client/components/ui/card"
 import { Collapsible, CollapsibleContent } from "@client/components/ui/collapsible";
 import { format } from "date-fns";
+import { Check, CircleEllipsis, FileSearch, Loader, X } from "lucide-react";
 import { useState } from "react";
 
+const claimStateMessages: Record<string, string> = {
+    "NOT_ASSIGNED": 'Belum Direview',
+    "ASSIGNED": 'Proses Review',
+    "ACCEPTED": 'Diterima',
+    "REJECTED": 'Ditolak'
+}
+const claimStateVarians: Record<string, BadgeProps["variant"]> = {
+    "NOT_ASSIGNED": "secondary",
+    "ASSIGNED": "default",
+    "ACCEPTED": "default",
+    "REJECTED": "destructive"
+}
+
+const claimStateIconProps = {
+    className: "mr-2",
+    size: 20
+};
+const claimStateIcons: Record<string, any> = {
+    "NOT_ASSIGNED": <Loader {...claimStateIconProps} />,
+    "ASSIGNED": <FileSearch {...claimStateIconProps} />,
+    "ACCEPTED": <Check {...claimStateIconProps} />,
+    "REJECTED": <X {...claimStateIconProps} />
+}
+
 function Claim({ data }: { data: any }) {
-    console.log(data);
     const [detail, setDetail] = useState(false);
 
     return <Card>
         <CardHeader>
             <CardTitle>{data.type}</CardTitle>
-            <CardDescription>{format(data.date, "dd MMMM yyyy")}</CardDescription>
+            <CardDescription>
+                {format(data.date, "dd MMMM yyyy")}
+                <Badge className="ml-2" variant={claimStateVarians[data.state]}>
+                    {claimStateIcons[data.state]}
+                    {claimStateMessages[data.state as string]}
+                </Badge>
+            </CardDescription>
         </CardHeader>
-        <CardContent>
-            <p>Status : {data.state}</p>
 
-            <Collapsible open={detail}>
-                <CollapsibleContent>
-                    <p>Rumah Sakit : {data.hospital}</p>
-                    <p>Deskripsi : {data.description}</p>
-                </CollapsibleContent>
-            </Collapsible>
-        </CardContent>
+        <Collapsible open={detail}>
+            <CollapsibleContent asChild>
+                <CardContent>
+                    <p className="text-sm font-bold">Rumah Sakit</p>
+                    <p>{data.hospital}</p>
+
+                    <p className="text-sm font-bold mt-4">Deskripsi</p>
+                    <p>{data.description}</p>
+                </CardContent>
+            </CollapsibleContent>
+        </Collapsible>
         <CardFooter>
             <Button onClick={() => setDetail((value) => !value)}>{detail ? "Urungkan Detail" : "Tampilkan Detail"}</Button>
         </CardFooter>
